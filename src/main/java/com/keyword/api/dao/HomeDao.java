21540da99338e362974920b1662917457cd5d9a8
DevDotUng <wkdu4510@gmail.com>
2022-08-08 21:16:05 +0900

    test

--- src/main/java/com/keyword/api/dao/HomeDao.java
+++ src/main/java/com/keyword/api/dao/HomeDao.java
@@ -3,10 +3,12 @@ package com.keyword.api.dao;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import com.keyword.api.entity.AppInfo;
 import com.keyword.api.entity.Screenshot;
 
+@Repository
 @Mapper
 public interface HomeDao {
 	List<AppInfo> getAppInfoList();
