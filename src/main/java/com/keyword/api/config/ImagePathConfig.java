8c53e57795639b197a910d4d5a740c2349672c74
DevDotUng <wkdu4510@gmail.com>
2022-08-21 19:53:59 +0900

    image config fix

--- src/main/java/com/keyword/api/config/ImagePathConfig.java
+++ src/main/java/com/keyword/api/config/ImagePathConfig.java
@@ -10,8 +10,11 @@ public class ImagePathConfig implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
-		registry.addResourceHandler("/image/icon/**").addResourceLocations("file:/Users/ung/Documents/Work/Tools/workspace-spring-tool-suite-4-4.15.1.RELEASE/KeywordAPI/src/main/webapp/static/upload/icon/");
-		registry.addResourceHandler("/image/screenshot/**").addResourceLocations("file:/Users/ung/Documents/Work/Tools/workspace-spring-tool-suite-4-4.15.1.RELEASE/KeywordAPI/src/main/webapp/static/upload/screenshot/");
+		// registry.addResourceHandler("/image/icon/**").addResourceLocations("file:/Users/ung/Documents/Work/Tools/workspace-spring-tool-suite-4-4.15.1.RELEASE/KeywordAPI/src/main/webapp/static/upload/icon/");
+		// registry.addResourceHandler("/image/screenshot/**").addResourceLocations("file:/Users/ung/Documents/Work/Tools/workspace-spring-tool-suite-4-4.15.1.RELEASE/KeywordAPI/src/main/webapp/static/upload/screenshot/");
+
+		registry.addResourceHandler("/image/icon/**").addResourceLocations("file:/home/ec2-user/apps/awsTest2/src/main/webapp/static/upload/icon/");
+		registry.addResourceHandler("/image/screenshot/**").addResourceLocations("file:/home/ec2-user/apps/awsTest2/src/main/webapp/static/upload/screenshot/");
 
 	}
 }
